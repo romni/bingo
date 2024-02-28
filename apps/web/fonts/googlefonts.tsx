a517f28d762a11f6f0e9df07707967a89971fc33
ohadn <ohad111@gmail.com>
2024-02-28 16:34:13 +0100

    Add yarn.lock and update dependencies
    A new yarn.lock file has been added to the project. This lock file will ensure consistent installation of the correct versions of dependencies across different environments. In addition, various dependencies listed in the file have been updated to the latest versions, resulting in enhanced performance and bug fixes.

---
+++ apps/web/fonts/googlefonts.tsx
@@ -0,0 +1,19 @@
+import {Rock_Salt, Caveat, Indie_Flower} from "next/font/google";
+
+export const rockSalt = Rock_Salt({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400']
+})
+
+export const caveat = Caveat({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const indieFlower = Indie_Flower({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400']
+})
+
