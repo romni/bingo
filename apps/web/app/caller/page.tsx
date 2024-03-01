b7a09e8ef71a89572533ceda811b1b027031327a
ohadn <ohad111@gmail.com>
2024-03-01 13:40:04 +0100

    Update game styling and add sayings feature
    Significant updates to the game interface have been made, including changes to styling and addition of a new 'sayings' feature. Now, there are randomly-selected sayings displayed during the game, adding more character to the experience. Features such as these enhance the game's fun and engagement factor for users. The UI changes include color adjustments, added padding in CSS modules and updated game name to 'Goommunity Bingomi'.

--- apps/web/app/caller/page.tsx
+++ apps/web/app/caller/page.tsx
@@ -1,11 +1,30 @@
 "use client"
 import React, {useRef, useState} from "react";
 import styles from "./page.module.css";
-import {caveat, indieFlower, rockSalt} from "web/fonts/googlefonts";
+import {rockSalt} from "web/fonts/googlefonts";
 import Button from "@repo/ui/button";
-import {it} from "node:test";
 
-const selectRandomItem = (gameArray: number[]) => Math.floor((Math.random() * gameArray.length))
+const sayings = [
+  "They don't make them like they used to.",
+  "In my time, we had to walk 10 miles to school in the snow.",
+  "You young whippersnappers have it so easy these days.",
+  "You call that music? That's just noise!",
+  "Back in the day, we respected our elders.",
+  "You need to get off that phone and read a book.",
+  "When I was your age...",
+  "You don't know how good you have it.",
+  "That's not how we did things in the old days.",
+  "What’s a simp? Is that a new kind of shrimp?",
+  "Kids these days have no manners.",
+  "Life was simpler then.",
+  "WHAT THE HELL IS A GYATT?!",
+  "Why do they keep saying ‘sus’ and ‘stan’? Can’t they speak proper English?",
+  "WHY IS THERE A SKIBIDI IN THE TOILET?!?!",
+  "What’s a pogger? Is that some kind of frog?",
+  "What’s an o7?"
+]
+
+const selectRandomItem = (gameArray: any[]) => Math.floor((Math.random() * gameArray.length))
 
 const getWinningNumber = (gameArray: number[]): { winningNumber: number, newGameArray: number[] } => {
   const selectedIndex = selectRandomItem(gameArray)
@@ -16,7 +35,6 @@ const getWinningNumber = (gameArray: number[]): { winningNumber: number, newGame
   }
 }
 
-
 const Page: React.FC = () => {
   const gameArray = Array.from({length: 99}, (_, i) => i + 1)
   const [remainingItems, setRemainingItems] = useState(gameArray)
@@ -24,11 +42,13 @@ const Page: React.FC = () => {
   const rollInterval = useRef<NodeJS.Timeout | null>(null)
   const [isRolling, setIsRolling] = useState(false)
   const [selectedNumber, setSelectedNumber] = useState<number>()
+  const [saying, setSaying] = useState<string | undefined>()
 
   const handleRoll = () => {
     if (!isRolling) {
       setIsRolling(true)
       const {winningNumber, newGameArray} = getWinningNumber(remainingItems)
+      setSaying(sayings[selectRandomItem(sayings)])
 
       rollInterval.current = setInterval(() => {
         const randomIndex = selectRandomItem(remainingItems)
@@ -41,6 +61,7 @@ const Page: React.FC = () => {
           rollInterval.current = null;
         }
         setSelectedItem(null)
+        setSaying(undefined)
         setRemainingItems(newGameArray)
         setSelectedNumber(winningNumber)
         setIsRolling(false)
@@ -50,13 +71,26 @@ const Page: React.FC = () => {
 
   return <main className={styles.main}>
     <h1 className={rockSalt.className}>Goommunity Bingomi</h1>
+    {
+      saying !== undefined &&
+      <h2 className={styles.sayings}>{saying}</h2>
+    }
+    {
+      selectedNumber === 69 && !isRolling &&
+      <h2 className={styles.sayings}>NICE!!</h2>
+    }
     <div className={styles.callerWrapper}>
       <div className={styles.resultWrapper}>
         <h2>
           {selectedNumber !== undefined && selectedNumber}
           {selectedNumber === 69 && ' Nice!'}
         </h2>
-        <Button disabled={isRolling} onClick={handleRoll}>Roll</Button>
+        <Button
+          disabled={isRolling}
+          onClick={handleRoll}
+        >
+          Roll
+        </Button>
       </div>
       <div className={styles.boardWrapper}>
         {
