c21bf6a75bd52f7118606a447d20a1feb7de89ee
romni <me@romnous.org>
2024-07-30 19:15:33 +0200

    reformat and update .gitignore

--- packages/ui/src/DesignControls.tsx
+++ packages/ui/src/DesignControls.tsx
@@ -33,60 +33,60 @@ const ColorControlWrapper = styled.div`
 `
 
 type DesignControlsProps = {
-  backgroundColor: string
-  backgroundColorHandler: (color: string) => void
-  fontColor: string
-  fontColorHandler: (color: string) => void
-  headerColor: string
-  headerColorHandler: (color: string) => void
-  borderColor: string
-  borderColorHandler: (color: string) => void
+    backgroundColor: string
+    backgroundColorHandler: (color: string) => void
+    fontColor: string
+    fontColorHandler: (color: string) => void
+    headerColor: string
+    headerColorHandler: (color: string) => void
+    borderColor: string
+    borderColorHandler: (color: string) => void
 
 } & React.ComponentPropsWithoutRef<'div'>
 
 const DesignControls: React.FC<DesignControlsProps> = (
-  {
-    backgroundColor,
-    backgroundColorHandler,
-    borderColor,
-    borderColorHandler,
-    fontColor,
-    fontColorHandler,
-    headerColor,
-    headerColorHandler,
-    ...props
-  }) => {
+    {
+        backgroundColor,
+        backgroundColorHandler,
+        borderColor,
+        borderColorHandler,
+        fontColor,
+        fontColorHandler,
+        headerColor,
+        headerColorHandler,
+        ...props
+    }) => {
 
-  return <ColorControlWrapper {...props}>
-    <div>
-      <h2>Background color</h2>
-      <HexColorPicker
-        color={backgroundColor}
-        onChange={backgroundColorHandler}
-      />
-    </div>
-    <div>
-      <h2>Border color</h2>
-      <HexColorPicker
-        color={borderColor}
-        onChange={(event) => borderColorHandler(event)}
-      />
-    </div>
-    <div>
-      <h2>Header color</h2>
-      <HexColorPicker
-        color={headerColor}
-        onChange={(event) => headerColorHandler(event)}
-      />
-    </div>
-    <div>
-      <h2>Font color</h2>
-      <HexColorPicker
-        color={fontColor}
-        onChange={(event) => fontColorHandler(event)}
-      />
-    </div>
-  </ColorControlWrapper>
+    return <ColorControlWrapper {...props}>
+        <div>
+            <h2>Background color</h2>
+            <HexColorPicker
+                color={backgroundColor}
+                onChange={backgroundColorHandler}
+            />
+        </div>
+        <div>
+            <h2>Border color</h2>
+            <HexColorPicker
+                color={borderColor}
+                onChange={(event) => borderColorHandler(event)}
+            />
+        </div>
+        <div>
+            <h2>Header color</h2>
+            <HexColorPicker
+                color={headerColor}
+                onChange={(event) => headerColorHandler(event)}
+            />
+        </div>
+        <div>
+            <h2>Font color</h2>
+            <HexColorPicker
+                color={fontColor}
+                onChange={(event) => fontColorHandler(event)}
+            />
+        </div>
+    </ColorControlWrapper>
 }
 
 export default DesignControls
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,31 +3,31 @@
 import React from "react";
 import {styled} from "styled-components";
 
-const ButtonWrapper = styled.button<{disabled?: boolean}>`
+const ButtonWrapper = styled.button<{ disabled?: boolean }>`
     height: 37px;
     display: flex;
     align-items: center;
     justify-content: center;
     cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
-    
+
     padding: 8px;
     border-radius: 2px;
     border: 1px solid gray;
     color: ${props => props.disabled && 'gray'};
-    
+
     &:hover {
         ${props => !props.disabled && 'background: rgba(0, 0, 0, 0.08)'};
     }
 `
 
 type ButtonProps = {
-  children: React.ReactNode
+    children: React.ReactNode
 } & React.ComponentPropsWithoutRef<'button'>
 
 const Button: React.FC<ButtonProps> = ({children, ...props}) => {
 
-  return <ButtonWrapper disabled={props.disabled} {...props}>
-    {children}
-  </ButtonWrapper>
+    return <ButtonWrapper disabled={props.disabled} {...props}>
+        {children}
+    </ButtonWrapper>
 }
 export default Button
