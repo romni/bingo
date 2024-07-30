13c7bda3172a4e6f75dba0b5e7be3295636a7829
romni <me@romnous.org>
2024-07-30 20:06:10 +0200

    fix user card styling

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,20 +3,21 @@
 import styles from "./page.module.css";
 import React, {useEffect, useState} from "react";
 import Card from "@repo/ui/card";
+import * as values from '../values.json'
 
-const shuffleArray = (array: number[]) => {
+const shuffleArray = (array: string[]) => {
   return array.map(value => ({value, sort: Math.random()}))
     .sort((a, b) => a.sort - b.sort)
     .map(({value}) => value)
 }
 
 const getGameCard = () => {
-  const gameArray = Array.from({length: 99}, (_, i) => i + 1)
+  const gameArray = values.data
   const randomArray = shuffleArray(gameArray)
-  const gameCard = randomArray.slice(0, 25)
+  const gameCard = randomArray.slice(0, 16)
 
-  return gameCard.reduce((accumulator: number[][], value: number, index: number) => {
-    const chunkIndex = Math.floor(index / 5)
+  return gameCard.reduce((accumulator: string[][], value: string, index: number) => {
+    const chunkIndex = Math.floor(index / 4)
 
     if (!accumulator[chunkIndex]) {
       accumulator[chunkIndex] = []
